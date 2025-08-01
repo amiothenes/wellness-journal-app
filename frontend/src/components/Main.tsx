8f4466048d17ae0c00ff7c7fb1064e524342914f
Amiothenes <60746276+OctavianJulius@users.noreply.github.com>
2025-07-31 20:04:41 -0400

    feat: implement mood selector and entry textarea in Main component

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import '../styles/Main.css';
 
 function Main() {
+    const [currentMood, setCurrentMood] = React.useState<number>(5);
+    const [entry, setEntry] = React.useState();
+
+    const handleMood = (event: ChangeEvent<HTMLInputElement>) => {
+        setCurrentMood(parseInt(event.target.value));
+    }
+
   return (
-    <div className="App">
-        Hello from Main!
+    <div>
+        <p>{entry}</p>
+        <form>
+            <textarea rows={10} placeholder="What's on your mind?"></textarea>
+            <div className="mood-selector">
+                <span>1</span><input type="range"
+                                        min={1}
+                                        max={10}
+                                        step={1}
+                                        value={currentMood}
+                                        onChange={handleMood}
+                ></input><span>10</span>
+                <p>{currentMood}</p>
+            </div>
+            <button>Save</button>
+        </form>
     </div>
   );
 }
