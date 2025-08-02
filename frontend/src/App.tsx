e378802cd6ec4a135e93d2547d652e19736c80a6
Amiothenes <60746276+OctavianJulius@users.noreply.github.com>
2025-08-02 17:33:02 -0400

    added Main.tsx functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './styles/App.css';
 import Main from './components/Main'
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
+        <Sidebar></Sidebar>
         <Main></Main>
     </div>
   );
--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,19 +1,34 @@
-import React, {ChangeEvent} from 'react';
+import React, {ChangeEvent, FormEvent} from 'react';
 import '../styles/Main.css';
 
 function Main() {
     const [currentMood, setCurrentMood] = React.useState<number>(5);
-    const [entry, setEntry] = React.useState();
+    const [currentEntry, setCurrentEntry] = React.useState<string>();
+    const [savedEntry, setSavedEntry] = React.useState<string>();
 
     const handleMood = (event: ChangeEvent<HTMLInputElement>) => {
         setCurrentMood(parseInt(event.target.value));
     }
 
+    const handleCurrentEntry = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        setCurrentEntry(event.target.value);
+    }
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setSavedEntry(currentEntry);
+        setCurrentEntry("");
+    }
+
   return (
     <div>
-        <p>{entry}</p>
-        <form>
-            <textarea rows={10} placeholder="What's on your mind?"></textarea>
+        <p>{savedEntry}</p>
+        <form onSubmit={handleSubmit}>
+            <textarea rows={10}
+                        placeholder="What's on your mind?"
+                        onChange={handleCurrentEntry}
+                        value={currentEntry}>
+                        </textarea>
             <div className="mood-selector">
                 <span>1</span><input type="range"
                                         min={1}
